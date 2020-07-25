9aae126ec758ede908a8ea238fbc587882253d79
hirotaka.arimitsu <arimitu1030@yahoo.co.jp>
2020-07-25 13:16:21 +0000

    favorites

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,8 @@ class ApplicationController < ActionController::Base
   def counts(user)
     @count_microposts = user.microposts.count
     @count_followings = user.followings.count
-    @count_followers = user.followers.count    
+    @count_followers = user.followers.count
+    @count_favorites = user.favorite_microposts.count
   end
 
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: {maximum: 255 }
+  
+  has_many :favorites
+  has_many :like_user, through: :reverses_of_favorite, source: :user
 end
